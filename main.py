f9b905461e52627bbcf763148deb4fc460b30001
Brouilletv <Brouilletv@educ.colleg-montreal.qc.ca>
2024-10-02 10:15:24 -0400

    final

--- main.py
+++ main.py
@@ -7,29 +7,36 @@ import random as r
 
 
 def start():
-    print("J'ai choisi un nombre de 1 à 100.\nÀ vous de le deviner...")
-    devinette(r.randint(1, 100), 0)
+    borne = input(f"voulez vous choisir les borne? y/n")
+    if borne == "n":
+        minimum = 1
+        maximum = 100
+    else:
+        minimum = int(input(f"minimum?"))
+        maximum = int(input(f"maximum?"))
+    print(f"les borne sont {minimum} et {maximum}")
+    devinette(r.randint(minimum, maximum), 0)
 
 
 def devinette(nombre_rechercher, nombre_essaie):
     nombre_essaie += 1
-    essaie = int(input("Entrez votre essaie"))
+    essaie = int(input(f"Entrez votre essaie"))
     if essaie > nombre_rechercher:
-        print("le nombre est plus petit")
+        print(f"le nombre est plus petit")
         devinette(nombre_rechercher, nombre_essaie)
     elif essaie < nombre_rechercher:
-        print("le nombre est plus grand")
+        print(f"le nombre est plus grand")
         devinette(nombre_rechercher, nombre_essaie)
     elif essaie == nombre_rechercher:
-        print("Bonne réponse \nnombre d'essaie:", nombre_essaie)
+        print(f"Bonne réponse \nnombre d'essaie: {nombre_essaie}")
         nombre_rechercher = input("Quit? y/n")
         if nombre_rechercher == "n":
             start()
         else:
-            print("au revoir")
+            print(f"au revoir")
             quit()
     else:
-        print("error")
+        print(f"error")
         devinette(nombre_rechercher, nombre_essaie)
 
 
